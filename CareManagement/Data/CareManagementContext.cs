b624bc59a21b59346d81f6aad90aced075004c87
Melanie Ta <nngoc2701@gmail.com>
2023-03-23 16:28:41 -0700

    Add in memory database context

---
+++ CareManagement/Data/CareManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CareManagement.Models;
+
+namespace CareManagement.Data
+{
+    public class CareManagementContext : DbContext
+    {
+        public CareManagementContext (DbContextOptions<CareManagementContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CareManagement.Models.Class>? Class { get; set; }
+
+    }
+}
