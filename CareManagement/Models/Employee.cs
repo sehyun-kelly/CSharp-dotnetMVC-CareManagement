6d8e8911ed93c9198ec3e68ea8fb116b85f18fc1
Brad <bradleymasciotra@gmail.com>
2023-03-24 11:31:16 -0700

    adding shift, employee and qualification model

---
+++ CareManagement/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Range(0, int.MaxValue)]
+
+        public int EMPLOYEE_ID { get; set; }
+
+        [ForeignKey("QUALIFICATION")]
+        public int Q_ID { get; set; }
+        public virtual Qualification? QUALIFICATION { get; set; }
+    }
+}
---
+++ CareManagement/Models/Shift.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareManagement.Models
+{
+    public class Shift
+    {
+
+        [Key]
+       // [Range(0, int.MaxValue)]
+
+        public Guid S_ID { get; set; }
+
+        [Required]
+        [ForeignKey("EMPLOYEE")]
+        public int EMPLOYEE_ID { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        [Required]
+        [ForeignKey("EMPLOYEE_MANAGER")]
+        public int MANAGER_ID { get; set; }
+        public virtual Employee EMPLOYEE_MANAGER { get; set; }
+
+        [Required]
+        [Range(0,86399, ErrorMessage = "The start time must be between {1} and {2}")]
+        public TimeSpan START_TIME { get; set; }
+
+        [Required]
+        [Range(0, 86399, ErrorMessage = "The start time must be between {1} and {2}")]
+        public TimeSpan END_TIME { get; set; }
+
+
+        [Required]
+        public Boolean? SICK { get;set }
+
+    }
+}
