3bae51ffb9b201df627f0baee74ee011a5673329
Ryan Odribege <rodribege@my.bcit.ca>
2023-03-31 11:38:13 -0700

    fix merge conflicts; add Qualification model

--- CareManagement/Models/OM/Payroll.cs
+++ CareManagement/Models/OM/Payroll.cs
@@ -9,16 +9,12 @@ namespace CareManagement.Models.OM
 {
     public class Payroll
     {
-        [Key]
-        public Guid PayrollID { get; set; }
+        [Key] public Guid PayrollID { get; set; }
 
-        [Required]
-        [ForeignKey("Employee")]
-        public Guid EmployeeId { get; set; }
+        [Required] [ForeignKey("Employee")] public Guid EmployeeId { get; set; }
         public virtual Employee? Employee { get; set; }
 
-        [Required]
-        public DateTime StartDate { get; set; }
+        [Required] public DateTime StartDate { get; set; }
 
         public DateTime? EndDate { get; set; }
 
@@ -26,37 +22,28 @@ namespace CareManagement.Models.OM
         [EnumDataType(typeof(EmploymentType))]
         public string? EmployeeType { get; set; }
 
-        [Required] 
-        [Range(0, int.MaxValue)] 
-        public int Hours { get; set; }
+        [Required] [Range(0, int.MaxValue)] public int Hours { get; set; }
 
-        [Required]
-        [Range(0, int.MaxValue)]
-        public int Overtime { get; set; }
+        [Required] [Range(0, int.MaxValue)] public int Overtime { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public int? LateDeduction { get; set; }
+        [Range(0, int.MaxValue)] public int? LateDeduction { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public int? VacationPay { get; set; }
+        [Range(0, int.MaxValue)] public int? VacationPay { get; set; }
 
 
-        [Range(0, int.MaxValue)]
-        public int? SickPay { get; set; }
+        [Range(0, int.MaxValue)] public int? SickPay { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public float? Pre_tax { get; set; }
+        [Range(0, int.MaxValue)] public float? Pre_tax { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public float? Tax { get; set; }
+        [Range(0, int.MaxValue)] public float? Tax { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public int? CheckAmount { get; set; }
+        [Range(0, int.MaxValue)] public int? CheckAmount { get; set; }
 
         public enum EmploymentType
-    {
-        F,
-        P,
-        O
+        {
+            F,
+            P,
+            O
+        }
     }
 }
---
+++ CareManagement/Models/OM/Qualification.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareManagement.Models.OM
+{
+    public class Qualification
+    {
+        [Key]
+        public Guid QualificationId { get; set; } // The Qualification ID
+
+        [Required]
+        [ForeignKey("Employee")]
+        public Guid EmployeeId { get; set; }
+
+    }
+}
