49e13b521b5e02f27d108ae3fc56ccead6a8512f
Monica Bacatan <monicab3002@outlook.com>
2023-03-31 10:41:44 -0700

    Removed unused code in selenium file

--- SeleniumTests.Tests/UnitTest1.cs
+++ SeleniumTests.Tests/UnitTest1.cs
@@ -2,61 +2,24 @@
 using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.DevTools.V109.CSS;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Edge;
-using System.Diagnostics;
 
 namespace SeleniumTests.Tests
 {
 	[TestClass]
 	public class UnitTest1
 	{
-		//[TestMethod]
-		//public void TestMethod1()
-		//{
-		//	//string url = "https://localhost:7121//Services";
-		//	//System.setProperty("webdriver.gecko.driver", "D:\\GeckoDriver\\geckodriver.exe");
-
-		//	//FirefoxDriver firefoxDriver = new FirefoxDriver();
-		//	//WebDriver driver = new EdgeDriver();
-
-		//	//ChromeDriver driver;
-
-		//	//ChromeOptions chromeOptions = new ChromeOptions();
-		//	//if (!Debugger.IsAttached)
-		//	//	chromeOptions.AddArguments("headless");
-		//	//chromeOptions.AddArguments("--start-maximized");
-		//	//driver = new ChromeDriver(chromeOptions);
-
-		//	ChromeDriver driver = new ChromeDriver();
-		//	//string url = "https://www.google.com//";
-		//	string url = "https://localhost:7121/Services";
-
-
-		//	driver.Manage().Window.Maximize();
-		//	driver.Navigate().GoToUrl(url);
-		//	driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-		//	//driver.FindElement(By.LinkText("Services")).Click();
-
-
-		//WebDriverManager.chromedriver().driverVersion("110.0.5481").setup();
-		//WebDriver web = new WebDriver(driver);
-
-		//}
-
 		[TestMethod]
 		public void TestService()
 		{
 			string url = "https://localhost:7121/Services";
 			ChromeDriver driver = new ChromeDriver();
 
+			// This redirects to the URL
 			driver.Manage().Window.Maximize();
 			driver.Navigate().GoToUrl(url);
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
+			// This tests creating a service
 			driver.FindElement(By.LinkText("Services")).Click();
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 			driver.FindElement(By.LinkText("Create New")).Click();
@@ -64,7 +27,6 @@ namespace SeleniumTests.Tests
 			driver.FindElement(By.Id("Hours")).SendKeys("8");
 			driver.FindElement(By.Id("Type")).SendKeys("service type");
 			driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
-
 
 			// This tests editing a service
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
@@ -76,13 +38,11 @@ namespace SeleniumTests.Tests
 			driver.FindElement(By.Id("Type")).SendKeys("type edited");
 			driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
 
-
 			// This tests viewing a service
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 			driver.FindElement(By.LinkText("Details")).Click();
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 			driver.FindElement(By.LinkText("Back to List")).Click();
-
 
 			// This tests deleting a service
 			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
