3781bce0fee4f629420ce170ba4b7a2e2eed48af
alexrpu <alexander.r.pu@gmail.com>
2023-04-05 23:21:50 -0700

    added total revenue per renter chart

--- CareManagement/Controllers/SCHDL/ReportController.cs
+++ CareManagement/Controllers/SCHDL/ReportController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CareManagement.Data;
 using CareManagement.Models.SCHDL;
+using CareManagement.Models.CRM;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace CareManagement.Controllers.SCHDL
 {
@@ -17,6 +20,25 @@ namespace CareManagement.Controllers.SCHDL
         public ReportController(CareManagementContext context)
         {
             _context = context;
+        }
+
+        //DataContract for Serializing Data - required to serve in JSON format
+        [DataContract]
+        public class DataPoint
+        {
+            public DataPoint(string label, double y)
+            {
+                this.Label = label;
+                this.Y = y;
+            }
+
+            //Explicitly setting the name to be used while serializing to JSON.
+            [DataMember(Name = "label")]
+            public string Label = "";
+
+            //Explicitly setting the name to be used while serializing to JSON.
+            [DataMember(Name = "y")]
+            public Nullable<double> Y = null;
         }
 
         // GET: Report
@@ -45,6 +67,33 @@ namespace CareManagement.Controllers.SCHDL
                 reports = reports.Where(s => DateTime.Compare(s.EndTime, (DateTime)endTime) <= 0);
             }
 
+
+            var invoices = from m in _context.Invoice.Include(r => r.Renter)
+                           select m;
+            var payments = await invoices.ToListAsync();
+
+            var renters = new List<Renter> { };
+            var dataPoints = new List<DataPoint>();
+
+            foreach (var invoice in invoices)
+            {
+                if (!renters.Contains(invoice.Renter)) {
+                    renters.Add(invoice.Renter);
+                    dataPoints.Add(new DataPoint(invoice.Renter.Name, invoice.TotalCost));
+                }
+                else
+                {
+                    foreach (var dataPoint in dataPoints)
+                    {
+                        if (dataPoint.Label.Contains(invoice.Renter.Name))
+                        {
+                            dataPoint.Y += invoice.TotalCost;
+                        }
+                    }
+                }
+            }
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
 
             return View(await reports.ToListAsync());
         }
