2a45a489a9e7d4c91a3f17b63c12c25166ead966
Monica Bacatan <monicab3002@outlook.com>
2023-04-05 18:40:05 -0700

    Created test for Invoice class

--- SeleniumTests.Tests/UnitTest1.cs
+++ SeleniumTests.Tests/UnitTest1.cs
@@ -104,49 +104,91 @@ namespace SeleniumTests.Tests
         }
 
 
-        //[TestMethod]
-        //public void TestInvoice()
-        //{
-        //    string urlInvoice = "https://localhost:7121/Invoices";
-        //    ChromeDriver driver = new ChromeDriver();
+        [TestMethod]
+        public void TestInvoice()
+        {
+            string urlInvoice = "https://localhost:7121/Invoices";
+            ChromeDriver driver = new ChromeDriver();
 
-        //    // This redirects to the Qualifications URL
-        //    driver.Manage().Window.Maximize();
-        //    driver.Navigate().GoToUrl(urlInvoice);
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            // This redirects to the Invoice URL
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(urlInvoice);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-        //    // This tests creating an invoice
-        //    driver.FindElement(By.LinkText("Create New")).Click();
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.Id("TotalHours")).SendKeys("30");
-        //    driver.FindElement(By.Id("TotalCost")).SendKeys("100");
-        //    driver.FindElement(By.Id("DatePaid")).Click();
+            // This tests creating an invoice
+            driver.FindElement(By.LinkText("Create New")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //driver.FindElement(By.Id("DatePaid")).Click();
+            //driver.FindElement(By.Id("StartDate")).Click();
 
-        //    driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+            // Send date and time to the DateTime element
+            IWebElement startDate = driver.FindElement(By.Id("StartDate"));
+            //Fill date as yyyy/mm/dd
+            driver.FindElement(By.Id("StartDate")).Click();
+            startDate.SendKeys("2023\t03-01");
+            //startDate.SendKeys("03 01");
+            //Press tab to shift focus to time field
+            //startDate.SendKeys(Keys.Tab);
+            //Fill time as 02:45 PM
+            startDate.SendKeys("0245PM");
+
+            IWebElement endDate = driver.FindElement(By.Id("EndDate"));            //Fill date as yyyy/mm/dd
+            driver.FindElement(By.Id("EndDate")).Click();
+            endDate.SendKeys("2023\t04-01");
+            endDate.SendKeys("0245PM");
+
+            IWebElement datePaid = driver.FindElement(By.Id("DatePaid"));
+            driver.FindElement(By.Id("DatePaid")).Click();
+            datePaid.SendKeys("2023\t04-01");
+            //datePaid.SendKeys("0600PM");
+
+            //IWebElement endDate = driver.FindElement(By.Id("EndDate"));
+            ////Fill date as yyyy/mm/dd
+            //endDate.SendKeys("2023-04-01");
+            ////Press tab to shift focus to time field
+            //endDate.SendKeys(Keys.Tab);
+            ////Fill time as 02:45 PM
+            //endDate.SendKeys("0245PM");
+
+            //IWebElement datePaid = driver.FindElement(By.Id("DatePaid"));
+            ////Fill date as yyyy/mm/dd
+            //datePaid.SendKeys("2023-04-01");
+            ////Press tab to shift focus to time field
+            //datePaid.SendKeys(Keys.Tab);
+            ////Fill time as 02:45 PM
+            //datePaid.SendKeys("0600PM");
 
 
 
+            //var today = DateTime.Today;
+            //driver.FindElement(By.Id("DatePaid")).Click();
+            //driver.FindElement(By.LinkText(today.Day.ToString())).Click();
 
-        //    // This tests editing a invoice
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.LinkText("Edit")).Click();
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.Id("QualificationDescription")).Clear();
 
-        //    driver.FindElement(By.Id("QualificationDescription")).SendKeys("QD3");
-        //    driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
 
-        //    // This tests viewing a invoice
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.LinkText("Details")).Click();
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.LinkText("Back to List")).Click();
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
 
-        //    // This tests deleting a invoice
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.LinkText("Delete")).Click();
-        //    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        //    driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
-        //}
+            // This redirects to the Invoice URL
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(urlInvoice);
+
+            // This tests editing a invoice
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Edit")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+
+            // This tests viewing a invoice
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Details")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Back to List")).Click();
+
+            // This tests deleting a invoice
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.LinkText("Delete")).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.FindElement(By.XPath("//Input[@type='submit']")).Click();
+        }
     }
 }
